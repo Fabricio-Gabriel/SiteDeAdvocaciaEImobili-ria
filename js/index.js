6613094bc9c5296d374e049e19bc19cc60799d94
wendell00 <wendellborges2003@gmail.com>
2023-04-14 23:24:50 -0300

    alterando O header e a Main

---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const nav = document.querySelectorAll('.liNav');
+    const image = document.querySelector('.logoImg');
+    const scrollTop = document.documentElement.scrollTop;
+  
+    if (scrollTop > 0) {
+
+      header.classList.add('scroll');
+      header.style.height = '80px'
+      for (var i = 0; i < nav.length; i++) {
+        nav[i].classList.add('scrollNav');
+      }
+      image.src = "../img/logoBlue.png";
+
+    } else {
+
+      header.classList.remove('scroll');
+      header.style.height = '110px'
+      for (var i = 0; i < nav.length; i++) {
+        nav[i].classList.remove('scrollNav');
+      }
+      image.src = "../img/logoWhite.png";
+
+    }
+  });
+
+var scroll = new SmoothScroll('a.scroll', {
+    speed: 800,
+    speedAsDuration: true
+});
